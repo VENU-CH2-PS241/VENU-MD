89a364e91d8efb0ee7feb32f6840e3b50957d517
Vanya <09021282126076@student.unsri.ac.id>
2023-12-17 22:31:33 +0700

    update ui

---
+++ app/src/main/java/com/capstone/venu/ui/detail_profile/DetailProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.capstone.venu.ui.detail_profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.venu.R
+
+class DetailProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_profile)
+    }
+}
